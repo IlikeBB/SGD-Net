730a7f1f12c0c176c5430a8a659683d28a576203
IlikeBB <32098079+IlikeBB@users.noreply.github.com>
2021-11-25 14:32:20 +0800

    Add files via upload

---
+++ SGDNet-pt_1.5&3.0/utils/S1_utils.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+from thop import profile
+from thop import clever_format
+
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    For calibrating misalignment gradient via cliping gradient technique
+    :param optimizer:
+    :param grad_clip:
+    :return:
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+
+def CalParams(model, input_tensor):
+    """
+    Usage:
+        Calculate Params and FLOPs via [THOP](https://github.com/Lyken17/pytorch-OpCounter)
+    Necessarity:
+        from thop import profile
+        from thop import clever_format
+    :param model:
+    :param input_tensor:
+    :return:
+    """
+    flops, params = profile(model, inputs=(input_tensor,))
+    flops, params = clever_format([flops, params], "%.3f")
+    print('[Statistics Information]\nFLOPs: {}\nParams: {}'.format(flops, params))
